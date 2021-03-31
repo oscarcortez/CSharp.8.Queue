7c5b05d738dcf525f429ee36f605da0ae9b374d7
oscarcortez <oscarkortez@gmail.com>
2021-03-31 18:39:43 -0400

    Add project files.

---
+++ ConsoleApp4Queue/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> numbers = new Queue<string>();
+            numbers.Enqueue("one");
+            numbers.Enqueue("two");
+            numbers.Enqueue("three");
+            numbers.Enqueue("four");
+            numbers.Enqueue("five");
+            numbers.Enqueue("six");
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine($"dequeue1 {numbers.Dequeue()}");
+            Console.WriteLine($"dequeue2 {numbers.Dequeue()}");
+            Console.WriteLine($"peek3: {numbers.Peek()}");
+            Console.WriteLine("------------");
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            
+        }
+    }
+}
